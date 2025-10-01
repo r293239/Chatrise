b4359c8c35f8b69f21cc36b4ecae7593cc8f6800
Reuben <reubenyee2012@gmail.com>
2025-10-01 11:36:15 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js - Configuration file
+// © 2025 [Reuben Yee]. All rights reserved.
+
+const CONFIG = {
+    PARSE_APP_ID: 'z5FgipCE12ScJNuYMbJ19EY2c7AXCxp5nWX7BWHT',
+    PARSE_JS_KEY: 'QNQTH3G4VuLA5gkPIiCtoXZjJJMcP7P5zYsETOPV',
+    PARSE_SERVER_URL: 'https://parseapi.back4app.com/',
+    EMAILJS_PUBLIC_KEY: 'deRsF9u2c6Z5ee142',
+    EMAILJS_SERVICE_ID: 'service_bh1l88z',
+    EMAILJS_TEMPLATE_ID: 'template_fchb4ci'
+};
